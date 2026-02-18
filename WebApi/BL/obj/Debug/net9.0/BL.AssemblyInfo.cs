9f5f3da4bddb31e3f919ec5ee8cb61ed0ea4b358
Abdullah Hashem <abdallahhashem326@gmail.com>
2026-02-18 02:18:17 +0200

    updated

--- WebApi/BL/obj/Debug/net9.0/BL.AssemblyInfo.cs
+++ WebApi/BL/obj/Debug/net9.0/BL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+460205b0670e94ce3dda259e2a34491e47dfcfe7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da8c38819c9cdb99fabb1102780498515a9b6a55")]
 [assembly: System.Reflection.AssemblyProductAttribute("BL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- WebApi/Domains/obj/Debug/net9.0/Domains.AssemblyInfo.cs
+++ WebApi/Domains/obj/Debug/net9.0/Domains.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domains")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+460205b0670e94ce3dda259e2a34491e47dfcfe7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da8c38819c9cdb99fabb1102780498515a9b6a55")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domains")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domains")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
